235478fc7bca75bc71a253fb95c66393b5a8ae9c
Henry Pendleton <hhpendleton@gmail.com>
2021-05-11 13:39:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here!
+const element = document.createElement("div");
+document.body.appendChild(element);
+
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+element.appendChild(ul);
+
+const main = document.querySelector("#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "YOUR-NAME is the champion";
